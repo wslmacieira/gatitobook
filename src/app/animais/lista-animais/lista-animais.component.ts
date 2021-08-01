30362c38e36710ffe24af7670be0982c877066b6
wslmacieira <wslmacieira@outlook.com>
2021-08-01 13:24:59 -0300

    utilizando rxjs

--- src/app/animais/lista-animais/lista-animais.component.ts
+++ src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { UsuarioService } from 'src/app/autenticacao/usuario/usuario.service';
 import { AnimaisService } from '../animais.service';
 import { Animais } from '../animal';
@@ -9,7 +11,7 @@ import { Animais } from '../animal';
   styleUrls: ['./lista-animais.component.css'],
 })
 export class ListaAnimaisComponent implements OnInit {
-  animais!: Animais;
+  animais$!: Observable<Animais>;
 
   constructor(
     private usuarioService: UsuarioService,
@@ -17,11 +19,17 @@ export class ListaAnimaisComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.usuarioService.retornaUsuario().subscribe((usuario) => {
-      const userName = usuario.name ?? '';
-      this.animaisService.listaDoUsuario(userName).subscribe((animais) => {
-        this.animais = animais;
-      });
-    });
+    // this.usuarioService.retornaUsuario().subscribe((usuario) => {
+    //   const userName = usuario.name ?? '';
+    //   this.animaisService.listaDoUsuario(userName).subscribe((animais) => {
+    //     this.animais = animais;
+    //   });
+    // });
+    this.animais$ = this.usuarioService.retornaUsuario().pipe(
+      switchMap((usuario) => {
+        const userName = usuario.name ?? '';
+        return this.animaisService.listaDoUsuario(userName);
+      })
+    )
   }
 }
