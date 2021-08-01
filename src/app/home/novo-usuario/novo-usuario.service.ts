6da2b058c37b2656d6c77178a6feaff59c120631
wslmacieira <wslmacieira@outlook.com>
2021-08-01 11:39:32 -0300

    environment

--- src/app/home/novo-usuario/novo-usuario.service.ts
+++ src/app/home/novo-usuario/novo-usuario.service.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { NovoUsuario } from './novo-usuario';
+
+const API = environment.apiURL;
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +14,10 @@ export class NovoUsuarioService {
   constructor(private http: HttpClient) { }
 
   cadastroNovousuario(novoUsario: NovoUsuario): Observable<NovoUsuario> {
-    return this.http.post<NovoUsuario>('http://localhost:3000/user/signup', novoUsario)
+    return this.http.post<NovoUsuario>(`${API}/user/signup`, novoUsario)
+  }
+
+  verificaUsuarioExistente(nomeUsario: string) {
+    return this.http.get(`${API}/user/exists/${nomeUsario}`)
   }
 }
