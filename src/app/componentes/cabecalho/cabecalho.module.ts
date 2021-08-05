a397b9a8d608e02582a6d3c9ebf55dfc9329a46d
wslmacieira <wslmacieira@outlook.com>
2021-08-05 08:18:32 -0300

    menu

--- src/app/componentes/cabecalho/cabecalho.module.ts
+++ src/app/componentes/cabecalho/cabecalho.module.ts
@@ -2,10 +2,11 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CabecalhoComponent } from './cabecalho.component';
+import { MenuModule } from '../menu/menu.module';
 
 @NgModule({
   declarations: [CabecalhoComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MenuModule],
   exports: [CabecalhoComponent],
 })
 export class CabecalhoModule {}
